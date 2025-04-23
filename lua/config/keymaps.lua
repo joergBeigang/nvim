041af6a575da1f70fe325f9bacdc948127928282
joergBeigang <51155962+joergBeigang@users.noreply.github.com>
2025-04-23 11:30:04 +0200

    .

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local t_oopts = { silent = true }
+-- normal mode
+
 -- insert mode
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 -- terminal mode
 vim.keymap.set("t", "<ESC>", "<C-\\><C-N>", t_oopts)
+vim.keymap.set("t", "jj", "<C-\\><C-N>", t_oopts)
 local job_id = 0
 vim.keymap.set("n", "<space>t", function()
     vim.cmd.vnew()
@@ -19,4 +22,8 @@ end)
 vim.keymap.set("n", "<space>run", function()
     vim.fn.chansend(job_id, { "source ~/dev/grblmachinist/.venv/bin/activate\r\n" })
     vim.fn.chansend(job_id, { "python ~/dev/grblmachinist/src/main.py\r\n" })
+end)
+
+vim.keymap.set("n", "<space>venv", function()
+    vim.fn.chansend(job_id, { "source ~/dev/grblmachinist/.venv/bin/activate\r\n" })
 end)
